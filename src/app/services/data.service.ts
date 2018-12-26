e13a8f99a8f2a6f7d83cfaebfdd1122eca282a59
ivan <ibodnar@outlook.com>
2018-12-26 13:30:02 -0300

    Agregado getUsers

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  baseUrl = environment.baseUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  intit() { }
+
+  getUsers(): Observable<any> {
+    return this.http.get<any>( this.baseUrl + '?results=5' );
+  }
 }
