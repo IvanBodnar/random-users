c7e7ec782f5c12e92e0733a804b0d13644e14e9a
ivan <ibodnar@outlook.com>
2018-12-26 15:00:14 -0300

    Se instancian los UserModel en getUsers

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,6 +3,9 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 import {environment} from '../../environments/environment';
+import {map} from 'rxjs/operators';
+import UserModel from '../models/user.model';
+import UserDetailsModel from '../models/user-details.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +19,26 @@ export class DataService {
 
   intit() { }
 
-  getUsers(): Observable<any> {
-    return this.http.get<any>( this.baseUrl + '?results=5' );
+  getUsers(): Observable<UserModel[]> {
+    return this.http.get<any>( this.baseUrl + '?results=5' )
+      .pipe(
+        map(
+          response => {
+            return response.results.map(
+              item => new UserModel(
+                item.name.first,
+                item.name.last,
+                item.location.street,
+                item.phone,
+                item.picture.thumbnail,
+                new UserDetailsModel(
+                  item.gender,
+                  item.email
+                )
+              )
+            );
+          }
+        )
+      );
   }
 }
