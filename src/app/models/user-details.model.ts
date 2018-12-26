d8c1e37257ac3be6dd62b626323a02918d07536c
ivan <ibodnar@outlook.com>
2018-12-26 20:51:45 -0300

    Auto stash before revert of "Se agregan campos a UserDetailModel"
    Se agregan campos a UserDetailsModel

--- src/app/models/user-details.model.ts
+++ src/app/models/user-details.model.ts
@@ -2,6 +2,9 @@
 export default class UserDetailsModel {
   constructor(
     public gender: string,
-    public email: string
+    public email: string,
+    public dob: string,
+    public age: number,
+    public picture: string
   ) { }
 }
