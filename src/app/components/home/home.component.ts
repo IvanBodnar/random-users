7d499a2abd0596b00f892e313c112f9e90a9e5b1
ivan <ibodnar@outlook.com>
2018-12-26 13:32:01 -0300

    Se consume getUsers en HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {DataService} from '../../services/data.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dataService: DataService
+  ) { }
 
   ngOnInit() {
+    this.dataService.getUsers()
+      .subscribe(
+        data => console.log(data),
+        err => console.log(err)
+      );
   }
 
 }
