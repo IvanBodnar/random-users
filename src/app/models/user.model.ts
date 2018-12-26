eb71ceea2810ee0f9ede450ac09c34fb6817a53b
ivan <ibodnar@outlook.com>
2018-12-26 20:50:39 -0300

    Se agregan métodos a UserModel

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -10,4 +10,16 @@ export default class UserModel {
     public thumbnail: string,
     public details: UserDetailsModel
   ) { }
+
+  private capitalize( word: string ): string {
+    return word.replace( word[0], word[0].toUpperCase() );
+  }
+
+  fullName(): string {
+    return `${ this.capitalize( this.firstName ) } ${ this.capitalize( this.lastName ) }`;
+  }
+
+  twitter(): string {
+    return `@${ this.firstName[0] }${ this.lastName }`;
+  }
 }
